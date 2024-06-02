978b7a3ed2b1a3a99168e3777e6dd216ea85f69f
DonShanilka <shanilka799@gmail.com>
2024-06-02 10:24:39 -0700

    add

--- model/OrderDetailsModel.js
+++ model/OrderDetailsModel.js
@@ -1,8 +1,9 @@
-export class orderdModel {
- constructor(order_id, item_id, price, qty) {
-  this.order_id = order_id;
-  this.item_id = item_id;
-  this.price = price;
-  this.qty = qty;
- }
+export class orderModel{
+    constructor(order_id,item_id,price,qty) {
+        this.order_id=order_id;
+        this.item_id=item_id;
+        this.price=price;
+        this.qty=qty;
+    }
+
 }
